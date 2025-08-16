772702068ae767cce953dac3392199099d1d71bc
Minibooga <silverclaw287@protonmail.com>
2025-08-16 16:07:24 +0000

    Create login.py

---
+++ login.py
@@ -0,0 +1,11 @@
+def login():
+  global usrname
+  global cfp
+  usrname=input("Please input your username: ")
+  pwd=input("Please input your password: ")
+  cfp=inpuut("Reenter your password: ")
+  if cfp==pwd:
+    print(Thank you.)
+  else:
+    print(PASSWORDS DO NOT MATCH)
+    login()
