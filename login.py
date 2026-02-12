40e4ada8fb453fb4a4c1a603b9cc9a37668f3c12
Minibooga <silverclaw287@protonmail.com>
2026-02-12 16:37:15 -0500

    Implement lockscreen function for user authentication
    Added lockscreen function to handle user login and password verification.

--- login.py
+++ login.py
@@ -6,6 +6,23 @@ def login():
   cfp=input("Reenter your password: ")
   if cfp==pwd:
     print("Thank you.")
+    lockscreen()
   else:
     print("PASSWORDS DO NOT MATCH")
     login()
+
+def lockscreen():
+  print("Welcome to ALLM OS")
+  un=input("Enter your username: ")
+  pw=input("Enter " usrname "'s password: ")
+  if pw==cfp:
+    print("Welcome to ALLM OS!")
+    allmos()
+  else:
+    print("Invalid username or password.")
+    lockscreen()
+
+
+
+
+login()
